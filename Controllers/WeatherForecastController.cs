3eebc58fd4d449fe0753f41b4b532e00a40d6acf
danihagosbl <168411661+danihagosbl@users.noreply.github.com>
2024-09-04 10:24:29 +0100

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace EmpApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Cloudy", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Cloudy", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
